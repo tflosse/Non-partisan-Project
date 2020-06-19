a1872f42b394d2bebb4011ef8099f341c9457648
Tamara Vahinerii Flosse <tflosse@tamaras-mbp.home>
2020-06-18 20:44:02 -0700

    Updates

---
+++ precache-manifest.7ee6372d22db86482ff9ec7796ed94f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0cd0500cff232c04a729fcab587e27b",
+    "url": "/npe-project/index.html"
+  },
+  {
+    "revision": "ade0efc0b82b9a07bc5c",
+    "url": "/npe-project/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f307831fcb491d64f591",
+    "url": "/npe-project/static/js/2.3ce9a7bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/npe-project/static/js/2.3ce9a7bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ade0efc0b82b9a07bc5c",
+    "url": "/npe-project/static/js/main.5f5e12d0.chunk.js"
+  },
+  {
+    "revision": "b35f24f94c76bd47cf7c",
+    "url": "/npe-project/static/js/runtime-main.1bb42a7b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/npe-project/static/media/logo.5d5d9eef.svg"
+  }
+]);
